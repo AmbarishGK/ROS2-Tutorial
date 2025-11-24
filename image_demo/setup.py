8823d4dabf07c96330780032ce5a887cc131055c
AmbarishGK <ambarishgk@gmail.com>
2025-11-24 11:41:30 -0800

    Removed yolo uncessary changes

--- image_demo/setup.py
+++ image_demo/setup.py
@@ -1,32 +1,30 @@
-from setuptools import find_packages, setup
+from setuptools import setup
 
 package_name = 'image_demo'
 
 setup(
     name=package_name,
     version='0.0.0',
-    packages=find_packages(exclude=['test']),
+    packages=[package_name],
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
+        ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        ('share/' + package_name + '/launch', ['launch/yolo_demo.launch.py']),
+        ('share/' + package_name + '/rviz', ['rviz/yolo_image.rviz']),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='mab',
-    maintainer_email='ambarishgk@gmail.com',
-    description='TODO: Package description',
-    license='TODO: License declaration',
-    extras_require={
-        'test': [
-            'pytest',
-        ],
-    },
+    maintainer_email='example@example.com',
+    description='Image demo package with video publisher and YOLO detector.',
+    license='MIT',
+    tests_require=['pytest'],
     entry_points={
         'console_scripts': [
             'image_filler_publisher = image_demo.image_filler_publisher:main',
             'image_relay = image_demo.image_relay:main',
             'video_publisher = image_demo.video_publisher:main',
+            'video_yolo = image_demo.video_yolo:main',
         ],
     },
 )
